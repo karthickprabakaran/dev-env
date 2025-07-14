33397943489e9aa2a46971659be283c8e5c42ecd
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-07-14 23:57:15 +0530

    Added the Alabster them

--- nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/lua/josean/plugins/colorscheme.lua
@@ -1,68 +1,34 @@
 return {
-  "Mofiqul/vscode.nvim",
-  priority = 1000,
-  config = function()
-    local transparent = true -- set to true if you would like to enable transparency
-    
-    require("vscode").setup({
-      -- Alternatively set style in setup
-      style = "dark", -- dark or light
-      
-      -- Enable transparent background
-      transparent = transparent,
-      
-      -- Enable italic comment
-      italic_comments = true,
-      
-      -- Underline `@markup.link.*` variants
-      underline_links = true,
-      
-      -- Disable nvim-tree background color
-      disable_nvimtree_bg = true,
-      
-      -- Override colors (see ./lua/vscode/colors.lua)
-      color_overrides = {
-        vscLineNumber = "#858585",
-      },
-      
-      -- Override highlight groups (see ./lua/vscode/theme.lua)
-      group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
-        Cursor = { fg = "#FFFFFF", bg = "#000000", bold = true },
-      }
-    })
-    
-    require("vscode").load()
-    
-    -- Additional transparency settings for better coverage
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      pattern = "*",
-      callback = function()
-        local groups = {
-          "Normal",
-          "NormalNC", 
-          "SignColumn",
-          "Folded",
-          "FoldColumn",
-          "CursorLine",
-          "CursorColumn",
-          "WhichKeyFloat",
-          "Pmenu",
-          "PmenuSbar",
-          "NormalFloat",
-          "FloatBorder",
-          "TelescopeNormal",
-          "TelescopeBorder",
-          "TelescopePromptBorder",
-          "TelescopeResultsBorder",
-          "TelescopePreviewBorder",
-        }
-        
-        for _, group in pairs(groups) do
-          vim.cmd(string.format("highlight %s guibg=NONE ctermbg=NONE", group))
-        end
-      end,
-    })
-  end,
+  {
+    "p00f/alabaster.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Enable true color support
+      vim.opt.termguicolors = true
+
+      -- Optional: force background mode
+      vim.opt.background = "dark" -- or "light"
+
+      -- Alabaster options
+      vim.g.alabaster_dim_comments = false
+      vim.g.alabaster_floatborder = false
+
+      -- Load theme
+      vim.cmd("colorscheme alabaster")
+
+      -- 🌿 Minimal Treesitter highlight overrides
+      local function set_highlight(group, opts)
+        vim.api.nvim_set_hl(0, group, opts)
+      end
+
+      -- Soft contrast without distracting from code
+      set_highlight("@function",  { fg = "#88C0D0", bold = false })  -- pale blue
+      set_highlight("@variable",  { fg = "#ECEFF4" })                -- light gray
+      set_highlight("@parameter", { fg = "#A3BE8C", italic = true }) -- soft green
+      set_highlight("@property",  { fg = "#D8DEE9" })                -- light gray
+      set_highlight("@keyword",   { fg = "#B48EAD", italic = true }) -- lavender
+      set_highlight("@comment",   { fg = "#5c6370", italic = true }) -- dimmed
+    end,
+  },
 }
