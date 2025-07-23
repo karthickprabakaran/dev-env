d47578a1f61035cbe22532dfef9ae012181faccf
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-07-23 17:01:17 +0530

    Update colorscheme.lua added some colour tweaks

--- nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/lua/josean/plugins/colorscheme.lua
@@ -1,4 +1,3 @@
-
 return {
   {
     "AlexvZyl/nordic.nvim",
@@ -8,13 +7,18 @@ return {
     config = function()
       require("nordic").setup({
         transparent_bg = true,
-        bold_keywords = false,
+        bold_keywords = true,
         italic = {
           comments = true,
-          strings = false,
-          keywords = false,
         },
         override = {
+          -- Add vibrancy by adjusting colors
+          Keyword = { fg = "#81A1C1", bold = true },     -- brighter blue
+          Function = { fg = "#8FBCBB" },                  -- teal
+          String = { fg = "#A3BE8C" },                    -- green
+          Constant = { fg = "#D08770" },                  -- orange
+          Type = { fg = "#B48EAD" },                      -- purple
+          -- Transparent background
           Normal = { bg = "NONE" },
           NormalNC = { bg = "NONE" },
           NormalFloat = { bg = "NONE" },
@@ -25,7 +29,6 @@ return {
 
       vim.cmd("colorscheme nordic")
 
-      -- Just in case: manually clear backgrounds (safe redundancy)
       vim.cmd([[
         hi Normal guibg=NONE ctermbg=NONE
         hi NormalNC guibg=NONE ctermbg=NONE
